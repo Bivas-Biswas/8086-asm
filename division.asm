724bd4777a7c58eaddae7423622d1c752c6d25ae
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-06-14 23:57:59 +0530

    update name

--- division.asm
+++ division.asm
@@ -1,8 +1,8 @@
 ;DIV S ' --->AX/S(STORE: Q=AL,R=AH)
               ;FOR S--->16 BIT, {DX,AX}/S=16 BIT 
                  ;Q-->AX,R-->DX
-MOV DX,1389H
-MOV AX,1004H 
+MOV DX,20H
+MOV AX,4H 
 MOV BX,1234H
 DIV BX
 HLT
